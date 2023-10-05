f06fcf015814abe261873b53a8db8fb4cd83c609
Chiagozie Eziagor <eziagorchy@gmail.com>
2023-10-05 19:20:05 +0000

    Print with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+  * main - Entry point of program
+  * printf: Print a given string
+  * Return: Always returns 0
+  */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
