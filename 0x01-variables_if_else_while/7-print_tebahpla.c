fae31e685d7cdcaced3dc3311e5634c47e9df9f5
Chiagozie Eziagor <eziagorchy@gmail.com>
2023-10-06 22:43:46 +0000

    Reverse lower case alphabets

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+  * main - Entry point of the program
+  * Return: Outputs 0
+  */
+int main(void)
+{
+	char z = 'z';
+
+	while (z <= 'a')
+	{
+		putchar('z');
+		z--;
+	}
+	putchar('\n');
+	return (0);
+}
