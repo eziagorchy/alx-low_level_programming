3246cfb3cf09be701d179bc48888025503e7b6a4
Chiagozie Eziagor <eziagorchy@gmail.com>
2023-10-11 03:16:02 +0000

    Print alphabet in a loop

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+  * print_alphabet_x10 - Function to print alphabet 
+  */
+void print_alphabet_x10(void)
+{
+	int loop_start = 0;
+	char a;
+
+	while (loop_start < 10)
+	{
+		a = 'a';
+
+		while (a <= 'z')
+		{
+			_putchar(a);
+			a++;
+		}
+		_putchar('\n');
+		loop_start++;
+	}
+}
