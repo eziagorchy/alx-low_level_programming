536ffc92c2d63bebbea4a40d1369cfe723da436d
Chiagozie Eziagor <eziagorchy@gmail.com>
2023-10-11 04:21:20 +0000

    Compute Absolute number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _abs -entry point
+ * @num: Number to be converted
+ * Return: returns 1;
+ */
+int _abs(int num)
+{
+	num = abs(num);
+	return (num);
+}
