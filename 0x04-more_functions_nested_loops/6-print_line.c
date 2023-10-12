20a5f782f96f5bd57039823dd854ad056606f50c
Chiagozie Eziagor <eziagorchy@gmail.com>
2023-10-12 16:53:17 +0000

    Print lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+  * print_line - Function name
+  * @n: Function argument
+  */
+void print_line(int n)
+{
+	while (n > 0)
+	{
+		_putchar('_');
+		n--;
+	}
+	_putchar('\n');
+}
