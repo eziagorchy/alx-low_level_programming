fc5eb9be78a35b598e761303797115262cfeb0c4
Chiagozie Eziagor <eziagorchy@gmail.com>
2023-10-12 14:52:22 +0000

    Print digits

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+  * print_numbers - Function name
+  */
+void print_numbers(void)
+{
+	start = 48;
+
+	while (start < 58)
+	{
+		_putchar(start);
+		start++;
+	}
+	_putchar('\n');
+}
+
