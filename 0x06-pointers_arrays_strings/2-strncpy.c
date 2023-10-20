59da213b8bc6b41df147c4ccdb00309b8b75bbe6
Chiagozie Eziagor <eziagorchy@gmail.com>
2023-10-20 04:31:19 +0000

    Custom strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,44 @@
+#include "main.h"
+/**
+  * _strncpy - Function name
+  * @dest: First parameter
+  * @src: Second parameter
+  * @n: Number of bytes to copy
+  * Return: Pointer to char
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+	int k = 0;
+	int zeros;
+
+	while (src[0] != '\0')
+		i++;
+	if (n <= i)
+	{
+		while (src[k] != '\0')
+		{
+			dest[j] = src[k];
+			j++;
+			k++;
+		}
+	}
+	else if (n > i)
+	{
+		zeros = n - i;
+
+		while (src[k] != '\0')
+		{
+			dest[j] = src[k];
+			j++;
+			k++;
+		}
+		while (zeros > 0)
+		{
+			dest[j] = '\0';
+			zeros--;
+		}
+	}
+	return (dest);
+}
