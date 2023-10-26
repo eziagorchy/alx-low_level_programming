e9cf2532c66033d39af45f745128e7e499745c5e
Chiagozie Eziagor <eziagorchy@gmail.com>
2023-10-26 08:05:33 +0000

    Handle edgecase

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -10,6 +10,8 @@ int wildcmp(char *s1, char *s2)
 	if (s2[0] == '\0' && s1[0] == '\0')
 		return (1);
 	else if (s2[0] == '\0')
+		return (0);
+	else if (s2[0] == '\0')
 	{
 		if (s2[0] == '*')
 			return (wildcmp(s1, s2 + 1));
