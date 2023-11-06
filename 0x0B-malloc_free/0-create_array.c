27963f2940dad4dd7037980824a71dff1f2b85c5
Chiagozie Eziagor <eziagorchy@gmail.com>
2023-11-06 19:05:45 +0000

    Dynamic allocation of space to create character array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+/**
+  * create_array - Function name
+  * @size: Size of array
+  * @c: Character to make up array
+  * Return: Returns a pointer to a char which is a character array
+  */
+char *create_array(unsigned int size, char c)
+{
+	char *char_arr;
+	int i = 0;
+
+	if (size == 0)
+		return (NULL);
+
+	char_arr = malloc(sizeof(*char_arr) * size);
+
+	if (char_arr == NULL)
+		return (NULL);
+	while (i < size)
+	{
+		char_arr[i] = c;
+		i++;
+	}
+	return (char_arr);
+}
