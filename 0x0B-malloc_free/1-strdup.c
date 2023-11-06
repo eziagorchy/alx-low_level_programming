f3c58e8172b612b5ef0d91417b04a2de47ef93dd
Chiagozie Eziagor <eziagorchy@gmail.com>
2023-11-06 19:53:16 +0000

    String duplicate

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+/**
+  * _strdup - Function name
+  * @str: String param to be duplicated
+  * Return: Returns a pointer to new string
+  */
+char *_strdup(char *str)
+{
+	char *new_str;
+	int i = 0;
+	int j = 0;
+
+	while (str[i] != '\0')
+		i++;
+	new_str = malloc(sizeof(char) * (i + 1));
+	if (i == 0 || new_str == NULL)
+		return (NULL);
+	while (j < (i + 1))
+	{
+		new_str[j] = str[j];
+		j++;
+	}
+	return (new_str);
+}
