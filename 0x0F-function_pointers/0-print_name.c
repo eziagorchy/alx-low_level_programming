074c8de5f02d7528c44e9cd975638708652cf710
Chiagozie Eziagor <eziagorchy@gmail.com>
2023-11-08 14:36:24 +0000

    Print name via function callback

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+/**
+  * print_name - Function name to print name
+  * @name: Name to print
+  * @f: Function pointer that accepts a string and returns nothing
+  * Return: Void
+  */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
