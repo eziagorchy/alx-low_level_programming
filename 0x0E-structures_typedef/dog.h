343aea9d1d3c8a7b72a4f39b5ac9d896d59ecf4f
Chiagozie Eziagor <eziagorchy@gmail.com>
2023-11-08 05:36:02 +0000

    Structure definintion

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+  * struct dog - Structure name
+  * @name: Structure member
+  * @age: Structure member
+  * @owner: Structure member
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
