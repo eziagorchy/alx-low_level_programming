31dfee0f2d1b89c8b54866ca96c2ba81740c2628
Chiagozie Eziagor <eziagorchy@gmail.com>
2023-11-08 09:36:26 +0000

    Include len function in main file

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -48,3 +48,16 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dogg->owner = dogg_owner;
 	return (dogg);
 }
+/**
+  * len_st - Function name
+  * @str: String to calculate the lenght
+  * Return: Return int
+  */
+int len_st(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+		i++;
+	return (i + 1);
+}
